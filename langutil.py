bc303e50bf7532ad7c04e10217dadba71d930049
caines <caines@22050313-c8de-4dc5-a602-147bd22adbe2>
2012-09-30 19:25:09 +0000

    Started work on code for scanning text for cross-references, etc.
    git-svn-id: file:///Users/caines/SVN/repos/XMLStatute/trunk@940 22050313-c8de-4dc5-a602-147bd22adbe2

--- langutil.py
+++ langutil.py
@@ -2,3 +2,8 @@
 
 """Module for provides functions for processing statute language (e.g., the applicability provisions of statutes, definitions, cross-references)."""
 
+import re
+
+class textParse(object):
+    def __init__(self, text):
+        self.text = text
