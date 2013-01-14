97005a9b885b589d7928bd3d339e6fbfa4b6e315
caines <ian.caines@gmail.com>
2013-01-13 23:57:35 -0500

    Added option for color output of warnings (currently red is the only option, if color parameter set to anything other than "None")

--- ErrorReporter.py
+++ ErrorReporter.py
@@ -4,8 +4,15 @@ import sys
 
 class StrictException(Exception): pass
 
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+
 STRICT = False #set to True to force an exception upon any warning message.
-def showError(s, header = "WARNING", location = None):
+def showError(s, header = "WARNING", location = None,color=None):
     if location is not None:
         while True:
             if hasattr(location, "getSectionLabel"): #work our way up the parent chain till we find something with a getSectionLabel that returns non-None           
@@ -18,4 +25,7 @@ def showError(s, header = "WARNING", location = None):
             pass
         pass
     if STRICT: raise StrictException(s)
-    else: sys.stderr.write(header + ": <" + s + ">\n")
+    else:
+        if color is not None: sys.stderr.write(FAIL)
+        sys.stderr.write(header + ": <" + s + ">\n")
+        if color is not None: sys.stderr.write(ENDC)
